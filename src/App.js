cabc3e90b6d310101403cf4e2d8b9d93ac055713
Dat <haruthong@gmail.com>
2021-04-29 16:55:13 -0700

    test fist page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,72 @@
+import ChatBot from 'react-simple-chatbot';
+import { ThemeProvider } from 'styled-components';
+import avatar from './Asset/62.png'
 
+import Test from "./Test";
 
 function App() {
+  const config = {
+    width: "500px",
+    botAvatar: `${avatar}`,
+};
+
+  const steps = [
+    {
+      id: '0',
+      component: <Test/>,
+      trigger: '1',
+    },
+    {
+      id: '1',
+      message: 'You can add custom components',
+      trigger: '2',
+    },
+     {
+        id: '2',
+        options: [
+          { value: 1, label: 'Number 1', component: <Test/>, trigger: '4' },
+          { value: 2, label: 'Number 2', trigger: '1' },
+          { value: 3, label: 'Number 3', trigger: '3' },
+        ],
+      },
+      {
+        id: '3',
+        message: 'Wrong answer, try again.',
+        trigger: '2',
+      },
+      {
+        id: '4',
+        message: 'Awesome! You are a telepath!',
+        end: true,
+      },
+    {
+      id: '5',
+      component: <Test/>,
+      end: true,
+    },
+  ];
+
+  const theme = {
+  background: '#F4f4f4',
+  fontFamily: 'Helvetica Neue',
+  headerBgColor: '#e4e4e4',
+  headerFontColor: '#4e78f6',
+  headerFontSize: '15px',
+  botBubbleColor: '#e4e4e4',
+  botFontColor: '#000',
+  userBubbleColor: '#fff',
+    userFontColor: '#4a4a4a',
+    rscHeader: "#fff",
+  };
+
+
+  const customProps = {
+    botAvatar: `${avatar}`,
+  }
   return (
-    <div>
-    <h1>hi</h1>
-    </div>
+      <ThemeProvider theme={theme}>
+        <ChatBot steps={steps}  {...config}/>
+        </ThemeProvider>
   );
 }
 
---
+++ src/Test.js
@@ -0,0 +1,13 @@
+import profileImage from "./Asset/61.png"
+
+const Test = () => {
+    return (
+        <div>
+            <img src={ profileImage} alt="HenryProfilePicture"/>
+            <h1>Henry</h1>
+            <p>TRANSFER SPECIALIST</p>
+        </div>
+    )
+}
+
+export default Test
